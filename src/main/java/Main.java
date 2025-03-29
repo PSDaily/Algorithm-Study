aaa515673ad3167c83f40cef2f5de85410b0c6fc
GNO CHOI <rmsghchl0@naver.com>
2025-03-30 01:29:40 +0900

    etc

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static int[] LIS;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.valueOf(br.readLine());
+        arr = new int[N];
+        LIS = new int[N];
+
+        StringTokenizer tokens = new StringTokenizer(br.readLine()," ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.valueOf(tokens.nextToken());
+        }
+
+        //when
+        Arrays.fill(LIS,Integer.MAX_VALUE);
+        LIS[0] = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            int num=arr[i];
+            int idx = Arrays.binarySearch(LIS, 0, i, num);
+            if(idx>0) continue;
+            if(idx<0) idx = -idx - 1;
+            LIS[idx]=num;
+        }
+
+        int result=0;
+        while (result < N && LIS[result] != Integer.MAX_VALUE) {
+            result++;
+        }
+        System.out.println(result);
+
+    }
+
+}
