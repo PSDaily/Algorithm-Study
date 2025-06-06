74c6c39cdae74dcc66e34ae0d40c930cb21a7b57
WithFortuna <rmsghchl0@naver.com>
2025-06-06 15:34:33 +0900

    20250606_1일_1알골
    재도전했고 풀었음.
    
    변경사항:
    - 그래프의 타입을 결정할 때 bfs내부에서 바로 결정하여 별도의 시간을 줄였음.
    - 또한 타입을 결정하는 기준으로 incoming, outgoing edge의 개수가 필요했으므로 이를 계산함
    - List.contains()는 역시나 많은 시간을 잡아먹음. => 별도의 array 변수를 이용함
    - 문제 조건상 노드개수가 4개여도 이름을 1~4로 안하고 2, 5, 6, 9도 가능했기에 좌표압축이 필요헀음

--- src/main/java/ProblemKakaoWinterInternship2.java
+++ src/main/java/ProblemKakaoWinterInternship2.java
@@ -2,68 +2,200 @@ import java.util.*;
 import java.io.*;
 
 class ProblemKakaoWinterInternship2 {
-    public int[] solution(int[][] edges) {
-        // List<List<Integer>> adjList = new ArrayList<>( );
-        int max=0;
+
+    public static void main(String[] args) {
+        int[][] edges = new int[][]{
+                {4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2},
+                {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10},
+                {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}
+        };
+        int[] result = solution(edges);
+        System.out.println(Arrays.toString(result));
+
+        /*
+        * int[][] array = {
+    {4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2},
+    {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10},
+    {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}
+};
+
+        * */
+    }
+
+    public static int[] solution(int[][] edges) {
+         List<List<Integer>> adjList = new ArrayList<>( );
+         Set<Integer> vertices = new HashSet<>();
+
+        //given
+//        int max=0;
+
+        int[] result=new int[4];
 
         for(int[] row: edges)
-            for(int col: row)
-                if(max<col) max=col;
+            for (int col : row) {
+                vertices.add(col);
+            }
+        // get numNode
+        int N = vertices.size();
+        for(int i=0;i<=N;i++)
+            adjList.add(new ArrayList<>());
+        // init adjList
+        List<Integer> temp = new ArrayList<>(vertices);
+        temp.add(-1);
+        Collections.sort(temp);
 
-        int N=max;
-        boolean[] isTarget=new boolean[N+1]; // 1 indexed, 막대의 루트 OR GNode
-        Arrays.fill(isTarget, true);
-
-        int targetCount=0;
-        int[] gnode=new int[2];
         for(int[] row: edges)
         {
-            int num=row[1];
-            isTarget[num]=false;
+            adjList.get(Collections.binarySearch(temp, row[0])).add(Collections.binarySearch(temp, row[1]));
         }
+
+
+
+        /*
+        타입1
+        incoming edge == outgoing edge == 1
+
+        type2
+        incoming edge =0 or outgoing edge=0인 node exists
+
+        type3
+        incoming edge == outgoing edge == 1초과 node exists
+
+        Gnode
+        incoming edge ==0 & outgoing edge의 수가 1이상임
+        */
+
+        // calculate incoming, outgoing edge
+        int[][] edgeStatus=new int[N+1][2]; //0: incoming, 1: outgoing
+        for(int[] row: edges)
+        {
+            int from = Collections.binarySearch(temp, row[0]);
+            int to = Collections.binarySearch(temp, row[1]);
+
+            edgeStatus[from][1]++;
+            edgeStatus[to][0]++;
+        }
+        // find Gnode
+        int gnode=0;
         for(int i=1;i<=N;i++)
-            if(isTarget[i]){
-                targetCount++;
-                gnode[gnode.length]=i;
-            }
-
-        // case1: 막대 그래프 1개만 있었던 경우
-        if(targetCount!=1)
         {
-            int target1=gnode[0];
-            int target2=gnode[1];
-
-        }
-        // case2: 그 외(루트가 확정되는 경우)
-        else{
-            //gnode확정
-            boolean[] visited=new boolean[N+1];
-
-            int realGnode=gnode[0];
-            int st=0;
-            if(st==realGnode) st++;
-
-            for(int i=st;i<=N;i++)
+            if(edgeStatus[i][0]!=0) continue;
+            if(edgeStatus[i][1]>1)
             {
-                if(visited[i])continue;
-                bfs(i);
+                gnode=i;
+                break;
+            }
+      /*      // 해당 노드로부터 탐색시작했을 때 모든 노드가 방문되어야함
+            boolean[] visited=new boolean[N+1];
+            boolean isGnode=true;
+            bfs2(i, visited);
+            for(int j=1;j<=N;j++)
+            {
+                if(!visited[j])
+                {
+                    isGnode=false;
+                    break;
+                }
+            }
+            if(!isGnode)continue;
+            gnode=i;*/
+        }
+
+        boolean[] isVisited2=new boolean[N+1];
+        isVisited2[gnode]=true;
+
+        boolean[] isChild=new boolean[N+1];
+        for (int child : adjList.get(gnode)) {
+            isChild[child]=true;
+        }
+        // 1. 막대 그래프 먼저 탐색
+        for(int i=1;i<=N;i++)
+        {
+            if(i==gnode)continue;
+            if (edgeStatus[i][0] == 0) {
+                bfs(i, isVisited2, result, edgeStatus,adjList);
+            } else if (edgeStatus[i][0]==1 && isChild[i]) {
+                bfs(i, isVisited2, result, edgeStatus,adjList);
             }
         }
+        // 2. 나머지 그래프 탐색
+        for(int i=1;i<=N;i++)
+        {
+               /*
+        타입1
+        incoming edge == outgoing edge == 1
+
+        type2
+        incoming edge =0 or outgoing edge=0인 node exists
+
+        type3
+        incoming edge == outgoing edge == 1초과 node exists
+
+        Gnode
+        incoming edge ==0 & outgoing edge의 수가 1이상임
+        */
+            if(i==gnode) continue;
+            if(isVisited2[i]) continue;
+            bfs(i, isVisited2, result, edgeStatus,adjList);
+
+
+        }
 
 
 
-        List<Integer> targets=new ArrayList<>();
-
-
-
-
-        int[] answer = {}; // Gnode 번호, 도넛 수 막대 수 ,8자 수
+        //!! 이떄 제출할 gnode는 압축된 gnode를 제공하면 안됨
+        int[] answer = {temp.get(gnode), result[1],result[2], result[3]}; // Gnode 번호, 도넛 수, 막대 수 ,8자 수
         return answer;
     }
 
-    public void bfs(int node){
-        //노드 완전탐색
-        // 그래프 타입 분류
-        // 그래프 타입 카운트+1
+    public static void bfs(int st, boolean[] isVisited, int[] result,int[][] edgeStatus, List<List<Integer>> adjList ){
+        //init
+        isVisited[st]=true;
+        Queue<Integer> q= new ArrayDeque<>();
+        q.offer(st);
+        int type=-1;
+
+        while(!q.isEmpty())
+        {
+            // 방문한 노드의 incoming, outgoing edge의 개수를 비교하여 결정노드라면 타입판별
+            int from=q.poll();
+            if(edgeStatus[from][1]==0) type=2;
+            if(edgeStatus[from][1]>1) type=3;
+
+            //search
+            for(int i=0;i<adjList.get(from).size(); i++)
+            {
+                int target=adjList.get(from).get(i);
+                if(!isVisited[target])
+                {
+                    isVisited[target]=true;
+                    q.offer(target);
+                }
+            }
+        }
+
+        if(type==-1) type=1;
+        result[type]++;
+    }
+    public static void bfs2(int st, boolean[] isVisited, List<List<Integer>> adjList){
+        //init
+        isVisited[st]=true;
+        Queue<Integer> q= new LinkedList<>();
+        q.offer(st);
+
+        while(!q.isEmpty())
+        {
+            int from=q.poll();
+            //search
+            for(int i=0;i<adjList.get(from).size(); i++)
+            {
+                int target=adjList.get(from).get(i);
+                if(!isVisited[target])
+                {
+                    isVisited[target]=true;
+                    q.offer(target);
+                }
+            }
+        }
     }
 }
