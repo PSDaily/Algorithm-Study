0872086fc992516d6f1036d2c947bf929f408b8d
GNO CHOI <rmsghchl0@naver.com>
2025-07-13 00:27:53 +0900

    20250713_복습_리스트
    Deque(양방향 삽입삭제 가능한 큐) 사용

--- src/main/java/Problem1158Re.java
+++ src/main/java/Problem1158Re.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Problem1158Re {
 
@@ -15,23 +12,23 @@ public class Problem1158Re {
 
         int N = Integer.valueOf(tokens.nextToken());
         int M = Integer.valueOf(tokens.nextToken());
-        List<Integer> ls = new LinkedList<>();
+        Deque<Integer> q = new ArrayDeque<>();
         for(int i=0;i<N;i++)
         {
-            ls.add(i+1);
+            q.offer(i+1);
         }
 
         //when
         int ptr = M - 1;
         List<Integer> result = new LinkedList<>();
         sb.append("<");
-        while (!ls.isEmpty()) {
-            result.add(ls.remove(ptr));
-            ptr += M - 1;
-            if (ptr >= ls.size() && !ls.isEmpty()) {
-                ptr = ptr % ls.size();
+        while (!q.isEmpty()) {
+            for (int i = 1; i <= M - 1; i++) {
+                q.offer(q.poll());
             }
+            result.add(q.poll());
         }
+
 
         for (Integer num : result) {
             if (num == result.get(N - 1)) {
