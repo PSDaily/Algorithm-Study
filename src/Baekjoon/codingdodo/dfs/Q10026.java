c49b4d11b638ae4e9055d763c87b9a7d04a33d26
mirupio <162397816+mirupio@users.noreply.github.com>
2025-10-07 15:15:51 +0900

    백준10026 리팩토링

--- src/Baekjoon/codingdodo/dfs/Q10026.java
+++ src/Baekjoon/codingdodo/dfs/Q10026.java
@@ -17,24 +17,25 @@ public class Q10026 {
             arr[i] = s.toCharArray();
         }
 
+        // normal
         boolean[][] visited = new boolean[N][N];
-
         int total = 0;
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
                 if(!visited[i][j]){
-                    dfsRecursive(i,j,visited,arr);
+                    dfsRecursive(i,j,visited,arr,false);
                     total++;
                 }
             }
         }
 
+        // blind
         boolean[][] visitedblindness = new boolean[N][N];
         int totalblindess = 0;
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
                 if(!visitedblindness[i][j]){
-                    dfsblindness(i,j,visitedblindness,arr);
+                    dfsRecursive(i,j,visitedblindness,arr,true);
                     totalblindess++;
                 }
             }
@@ -45,7 +46,7 @@ public class Q10026 {
 
     static int[] dx = {0,1,0,-1};
     static int[] dy = {-1,0,1,0};
-    static void dfsRecursive(int x, int y, boolean[][] visited, char[][] arr){
+    static void dfsRecursive(int x, int y, boolean[][] visited, char[][] arr, boolean isBlind){
         visited[x][y] = true; // 현재 노드 방문 처리
         // System.out.println("x:"+x+" y:"+y + " "); // 방문 출력
 
@@ -54,12 +55,15 @@ public class Q10026 {
         for(int i=0;i<4;i++){
             nextX = x + dx[i];
             nextY = y + dy[i];
-            if((nextX>=0 && nextX<N && nextY>=0 && nextY<N)&&(!visited[nextX][nextY]) &&(arr[x][y] == arr[nextX][nextY])){
-                dfsRecursive(nextX,nextY,visited,arr);
+            if((nextX>=0 && nextX<N && nextY>=0 && nextY<N)
+                    &&(!visited[nextX][nextY])
+                    &&(isSameColor(arr[x][y],arr[nextX][nextY],isBlind))){
+                dfsRecursive(nextX,nextY,visited,arr,isBlind);
             }
         }
     }
 
+    /*
     static void dfsblindness(int x, int y, boolean[][] visited, char[][] arr){
         visited[x][y] = true; // 현재 노드 방문 처리
         // System.out.println("x:"+x+" y:"+y + " "); // 방문 출력
@@ -83,5 +87,15 @@ public class Q10026 {
                 }
             }
         }
+    }
+     */
+
+    static boolean isSameColor(char color1, char color2, boolean isBlind){
+        if(isBlind){
+            if((color1 == 'R' || color1 == 'G') && (color2 == 'R' || color2 == 'G')) {
+                return true;
+            }
+        }
+        return color1 == color2;
     }
 }
